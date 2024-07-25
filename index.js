11fdfa79b20780bb885a6df3a647177e47b9b00a
Elvis <elvis.omondi@student.moringaschool.com>
2024-07-25 19:04:56 +0300

    commit

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-// Write your code here!
-
-window.newHeader = null; // declare newHeader as a global variable
+const userName = 'John'; // Replace with the actual user's name
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Remove the main element with id 'main'
   const main = document.querySelector('main#main');
-  main.remove();
+  if (main) {
+    main.remove();
+  }
 
-  // Create a new <h1> element and assign it to the newHeader variable
-  window.newHeader = document.createElement('h1');
-  window.newHeader.id = 'victory';
-  window.newHeader.textContent = 'John Doe is the champion'; // Replace with your actual name
-
-  // Add the newHeader element to the DOM
-  document.body.appendChild(window.newHeader);
+  newHeader = document.createElement('h1');
+  newHeader.textContent = `${userName} is the champion`;
+  newHeader.id = 'victory';
+  document.body.appendChild(newHeader);
 });
